9c570c6cff2131c58fa3a5bfcd371a3cbee85a21
abdwhb-png <winnerk088@gmail.com>
2026-07-05 21:23:14 +0100

    🧹 remove unused roleWidget import and code

--- agent/extensions/pi-roles-addons/index.ts
+++ agent/extensions/pi-roles-addons/index.ts
@@ -5,10 +5,8 @@
 import type { ExtensionAPI } from "@earendil-works/pi-coding-agent";
 import planAutoSwitch from "./plan-auto-switch.ts";
 import promptRoleSwitch from "./prompt-role-switch.ts";
-import roleWidget from "./role-widget.ts";
 
 export default function aldoborreroExtensions(pi: ExtensionAPI) {
   planAutoSwitch(pi);
   promptRoleSwitch(pi);
-  roleWidget(pi);
 }
